437a8b230e011cd47b9bd3aab95592e423fe5571
waelzahir <ozahir@student.1337.ma>
2023-04-27 18:58:32 +0100

    fix: bug when creating socket , instead of storing the fd in global (sock)

--- cp_dae.c
+++ cp_dae.c
@@ -7,22 +7,25 @@ int init_daemon()
 { 
     bzero(&server_a, sizeof(ADRESS));
     
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0)
         return (1);
     server_a.sin_family = AF_INET;
-    server_a.sin_port = 9000;
+    server_a.sin_port = 900;
     server_a.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (bind(sock,(void *)&server_a, sizeof(ADRESS)) < 0)
         return (close(sock), printf("error\n"), 1);
     printf("bind sucsessfull\n");
-    close(sock);
-
+    return (0);
 }
 int main (int ac, char  **av)
 {
     if (init_daemon())
         return (perror("\n"), 2);
-   
+    while (1)
+    {
+        if (listen(sock, 0))
+            return (perror(""), close(sock), 1);
+    }
 }
